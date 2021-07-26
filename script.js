b66f2ea5f0758505176053c98a070ee88b03f1d6
Diego-Justino-Ads <diegojustino6@gmail.com>
2021-07-25 21:04:05 -0300

    Versão 2.0

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ open.addEventListener('click', () => {
 close.addEventListener('click', () => {
     modal_container.classList.remove('show');
 });
+
+function validate() {
+    let x = document.forms["myForm"]["nome"].value;
+    if (x == "") {
+      alert("Nome deve ser preenchido");
+      return false;
+    }
+  }
